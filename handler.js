d80d00bbb4035b6ce7fb175714dfa7b0ad90cc02
Dave Burke <dburke84@gmail.com>
2020-09-11 22:51:21 -0500

    Implement SES to send email

--- handler.js
+++ handler.js
@@ -1,18 +1,51 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+var aws = require('aws-sdk');
+var ses = new aws.SES({region: 'us-east-1'});
+
+var { EMAIL_SENDER, EMAIL_RECIPIENT } = process.env;
+
+function sendEmail(recipient, subject, text) {
+  var params = {
+    Destination: {
+      ToAddresses: [recipient]
+    },
+    Message: {
+      Body: {
+        Text: { Data: text }
       },
-      null,
-      2
-    ),
+      Subject: { Data: subject }
+    },
+    Source: EMAIL_SENDER,
   };
 
+  return new Promise((resolve, reject) => {
+    ses.sendEmail(params, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+module.exports.notify = async event => {
+  try {
+    var emailResult = await sendEmail(EMAIL_RECIPIENT, 'Test subject', 'Test body');
+  } catch(err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err, null, 2),
+    };
+  }
+
+  console.log('Success');
+  return {
+    statusCode: 200,
+    body: JSON.stringify(emailResult, null, 2),
+  };
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
