fc38cec8c420ee4ad79afa0aa7877e50520ef6c6
Dave Burke <dburke84@gmail.com>
2020-09-12 22:22:15 -0500

    Code cleanup

--- handler.js
+++ handler.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var aws = require('aws-sdk');
-var ses = new aws.SES({region: 'us-east-1'});
+const aws = require('aws-sdk');
+const ses = new aws.SES({region: 'us-east-1'});
 
-var { EMAIL_SENDER, EMAIL_RECIPIENT } = process.env;
+const { EMAIL_SENDER, EMAIL_RECIPIENT } = process.env;
 
 function sendEmail(recipient, subject, text) {
-  var params = {
+  const params = {
     Destination: {
-      ToAddresses: [recipient]
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: { Data: text }
       },
-      Subject: { Data: subject }
+      Subject: { Data: subject },
     },
     Source: EMAIL_SENDER,
   };
 
   return new Promise((resolve, reject) => {
-    ses.sendEmail(params, function (err, data) {
+    ses.sendEmail(params, (err, data) => {
       if (err) {
         reject(err);
       } else {
@@ -32,20 +32,16 @@ function sendEmail(recipient, subject, text) {
 
 module.exports.notify = async event => {
   try {
-    var emailResult = await sendEmail(EMAIL_RECIPIENT, 'Test subject', 'Test body');
+    const emailResult = await sendEmail(EMAIL_RECIPIENT, 'Test subject', 'Test body');
+    return {
+      statusCode: 200,
+      body: JSON.stringify(emailResult, null, 2),
+    };
   } catch(err) {
     console.log(err);
     return {
       statusCode: 500,
-      body: JSON.stringify(err, null, 2),
+      body: JSON.stringify({ error: err, input: event }, null, 2),
     };
   }
-
-  console.log('Success');
-  return {
-    statusCode: 200,
-    body: JSON.stringify(emailResult, null, 2),
-  };
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
