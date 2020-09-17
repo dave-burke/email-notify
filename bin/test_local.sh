477e06da4f76afe99578d4aa0969467f7c3646a6
Dave Burke <dburke84@gmail.com>
2020-09-16 21:32:22 -0500

    Fix test request body

--- bin/test_local.sh
+++ bin/test_local.sh
@@ -7,7 +7,7 @@ request_body='{
 	"Content-Type": "application/json",
 	"Authorization": "Basic dXNlcjp0ZXN0"
   },
-  "body": "{ \"subject\": \"test notify '"${RANDOM}"'\", \"body\": \"test from sls\", \"passphrase\": \"test\" }"
+  "body": "{ \"subject\": \"test notify '"${RANDOM}"'\", \"body\": \"test from sls\" }"
 }'
 
 sls invoke local --aws-profile serverless -d "${request_body}" -f notify
